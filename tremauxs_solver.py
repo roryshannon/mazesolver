1e1073750b62fb49f9993fd86156d65e5e3a7e56
roryshannon <rorybshannon@gmail.com>
2021-12-15 22:16:27 +0000

    tremaux maerk as visited function

--- tremauxs_solver.py
+++ tremauxs_solver.py
@@ -3,6 +3,7 @@ import time
 class Tremauxs:
     def __init__(self):
         self.name = "tremaux"
+        self.visited = []
                
     def set_terminal_squares(self, start, end):
         self.start_square = start
@@ -54,51 +55,56 @@ class Tremauxs:
         print(f"at {self.current_square} tremuax can move to: {self.connections}")
         
         return self.connections
+    
+    def mark_as_visited(self):
+        self.visited.append(self.current_square)
         
     def make_move(self):
         degrees = 0
+        
         while self.current_square != self.end_square: #until we are at the end
             
-            if len(self.connections) > 2:
+            '''if len(self.connections) > 2:
                 print("at a junction! we must decide what to do!")
-                print(f"these are the possible connections: {self.connections}, but we have just come from {self.current_square-self.forward}")
+                print(f"these are the possible connections: {self.connections}, but we have just come from {self.current_square-self.forward}")'''
                 
-                time.sleep(100000)
-            
+                
+                
+            if self.current_square + self.forward in self.connections:
+                print("moving forward") #so the follower should go forward
+                if len(self.connections) > 2: #this runs if the square is a junction, and then adds the chose path into the visited list
+                    self.mark_as_visited()
+                    print(self.visited)
+                           
+                self.current_square = self.current_square + self.forward
+                self.connections = self.get_available_moves()
+                
+            elif self.current_square + self.right in self.connections:
+                print("moving right") #kinda irrelevant unless there is no move forward
+                self.current_square = self.current_square + self.right
+                self.connections = self.get_available_moves()
+                
+                degrees += - 90
+                if degrees < 0:
+                    degrees = 270 #circle for the degrees
+                self.set_direction(degrees) 
+                
+            elif self.current_square + self.left in self.connections:
+                print("moving left") #so the follower should turn left, as the wall has run out
+                self.current_square = self.current_square + self.left
+                self.connections = self.get_available_moves()
+                
+                degrees += 90
+                if degrees > 270:
+                    degrees = 0 #resetting degrees to 0 when a full circle has been completed
+                self.set_direction(degrees)
+                    
             else:
-                print("not a junction, we can continue!")
+                print("dead end!, retracing steps!")
+                #this is only important when we get back to the original junction!
                 
-                if self.current_square + self.forward in self.connections:
-                    print("moving forward") #so the follower should go forward
-                    self.current_square = self.current_square + self.forward
-                    self.connections = self.get_available_moves()
-                    
-                elif self.current_square + self.right in self.connections:
-                    print("moving right") #kinda irrelevant unless there is no move forward
-                    self.current_square = self.current_square + self.right
-                    self.connections = self.get_available_moves()
-                    
-                    degrees += - 90
-                    if degrees < 0:
-                        degrees = 270 #circle for the degrees
-                    self.set_direction(degrees) 
-                    
-                elif self.current_square + self.left in self.connections:
-                    print("moving left") #so the follower should turn left, as the wall has run out
-                    self.current_square = self.current_square + self.left
-                    self.connections = self.get_available_moves()
-                    
-                    degrees += 90
-                    if degrees > 270:
-                        degrees = 0 #resetting degrees to 0 when a full circle has been completed
-                    self.set_direction(degrees)
-                        
-                else:
-                    print("dead end!, retracing steps!")
-                    #need to do action here!
-                    time.sleep(1000000)
-                    '''degrees += 180
-                    if degrees > 270:
-                        degrees = 0 #resetting degrees to 0 when a full circle has been completed
-                    self.set_direction(degrees)'''
+                degrees += 180
+                if degrees > 270:
+                    degrees = 0 #resetting degrees to 0 when a full circle has been completed
+                self.set_direction(degrees)
                                    
